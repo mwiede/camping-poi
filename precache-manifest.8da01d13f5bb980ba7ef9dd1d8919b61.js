79d6d61499f617a1c49db833c5b81d5985568819
Wiedemann Matthias <matthias.wiedemann@swisslife.de>
2020-09-16 09:09:45 +0200

    Updates

---
+++ precache-manifest.8da01d13f5bb980ba7ef9dd1d8919b61.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a91219256a07245963a1d6ee4004c82b",
+    "url": "/camping-poi/index.html"
+  },
+  {
+    "revision": "e81aa5acbcdeca222da7",
+    "url": "/camping-poi/static/css/2.13d08433.chunk.css"
+  },
+  {
+    "revision": "4bf0a46865342ff646c4",
+    "url": "/camping-poi/static/css/main.0fed2176.chunk.css"
+  },
+  {
+    "revision": "e81aa5acbcdeca222da7",
+    "url": "/camping-poi/static/js/2.52232f42.chunk.js"
+  },
+  {
+    "revision": "a95b85f9ccb93cc381e54812b9aae3dc",
+    "url": "/camping-poi/static/js/2.52232f42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bf0a46865342ff646c4",
+    "url": "/camping-poi/static/js/main.dfdb54c5.chunk.js"
+  },
+  {
+    "revision": "66887380247251043d0d",
+    "url": "/camping-poi/static/js/runtime-main.a3ae130d.js"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/camping-poi/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/camping-poi/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/camping-poi/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/camping-poi/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/camping-poi/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/camping-poi/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/camping-poi/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/camping-poi/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/camping-poi/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/camping-poi/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/camping-poi/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/camping-poi/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/camping-poi/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/camping-poi/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/camping-poi/static/media/fa-solid-900.ee09ad75.woff"
+  }
+]);
